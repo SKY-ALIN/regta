cb48c63eeff7c429318b8e7c595f3795deb20f75
Vladimir Alinsky <adam3iks@gmail.com>
2022-02-17 00:33:53 +0800

    Update version

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ lint:
 	find . -iname "*.py" -not -path "./.venv/*" | xargs pylint
 test:
 	pytest
+html_docs:
+	cd docs/ && $(MAKE) clean && $(MAKE) html
--- regta/__init__.py
+++ regta/__init__.py
@@ -9,7 +9,7 @@ from .jobs import (
 )
 from .utils import run_jobs as run
 
-__version__ = '0.1.0a0'
+__version__ = '0.2.0'
 
 __all__ = [
     "Scheduler",
