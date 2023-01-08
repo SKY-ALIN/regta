25983e5a24d9e84ab3ea19d05600c1545bbc9860
Vladimir Alinsky <vladimir@alinsky.tech>
2023-01-08 16:04:35 +0600

    Bump package version (0.3.0)

--- regta/__init__.py
+++ regta/__init__.py
@@ -2,7 +2,7 @@ from .jobs import async_job, AsyncJob, process_job, ProcessJob, thread_job, Thre
 from .schedulers import Scheduler
 from .utils import run_jobs as run
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __all__ = [
     "Scheduler",
